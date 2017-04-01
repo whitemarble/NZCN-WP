4f1910c380efe03ee75d79e0f587d0e9fe798fd9
hawkroc <yinpengroc@gmail.com>
2017-04-01 17:30:43 +1300

    'merge'

--- single.php
+++ single.php
@@ -9,22 +9,28 @@
 	<content class="col-7 col-lg-7 main-content">
 
 <h1 class="post-content-title">
-	This is title
+	<?php the_title(); ?> 
 </h1>
 <p class="article-meta">
 by 
-<a href="">admin</a>
+<a href=""><?php the_title(); ?> </a>
  <i class="fa fa-clock-o" aria-hidden="true"></i> 
  <a href="">22/March/2017</a> 
- <i class="fa fa-comments-o" aria-hidden="true"></i> 
- <a href="">1 comment</a></p>
+<?php the_category(''); ?>
+ </p>
 <figure>
   <img src="https://ld-wp.template-help.com/wordpress_58404_v4/wp-content/uploads/2016/02/04.10.14_Obama-1170x780.jpg" style="width:100%;height:100%;"/>
 </figure>
 <article>
-	This is article blablabla
-	When people tell me that our President has delivered his 7th State of the Union speech recently, I always correct their mistake and tell it was eighth, in fact. On February 24, 2009, the start of Barack Obama’s 2nd month in office, he made an extensive speech on then ongoing Great Recession crisis. He tried to show what his personal vision for a path out was and why it was aligned with a $787 billion stimulus law that was passed that same day. Also, Mr. President talked foreign politics on that same occasion. He explained how well he will succeed in using diplomacy instead of Bush’s warmongering, strengthen old alliances and, all in all, spruce up US international image to a better one. He said: “living our values doesn’t make us weaker, it makes us safer and it makes us stronger.” Even GOPers were approving of the path POTUS has undertaken then…at least of the rhetorics style, which he so Hollywood-esquely told it in.
+<?php 
 
+if (have_posts()) :
+                while (have_posts()) :
+                    the_post();
+                    the_content();
+                endwhile;
+            endif;
+?>
 </article>
 
 	</content>
@@ -48,6 +54,5 @@ aside
                     <i class="fa fa-weibo" aria-hidden="true"></i>
                     <i class="fa fa-weixin" aria-hidden="true"></i>
 </ul>
-
 
 <?php get_footer(); ?>
