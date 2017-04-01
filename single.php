e6f21e825882b708b9e9c66186afdb907171281f
hawkroc <yinpengroc@gmail.com>
2017-04-01 16:35:44 +1300

    content

--- single.php
+++ single.php
@@ -1,4 +1,47 @@
 <?php get_header(); ?>
 
-<?php echo " <br> this is single <br>";?>
+    <div class="container-fluid main-section post-content">
+        <div class="row">
+<div  class="col-1 col-lg-1 main-content">
+
+</div>
+
+	<content class="col-7 col-lg-7 main-content">
+
+<h1 class="">
+	This is title
+</h1>
+<p class="article-meta">by <a href="">admin</a> <i class="fa fa-clock-o" aria-hidden="true"></i> <a href="">22/March/2017</a> <i class="fa fa-comments-o" aria-hidden="true"></i> <a href="">1 comment</a></p>
+<figure>
+  <img src="https://ld-wp.template-help.com/wordpress_58404_v4/wp-content/uploads/2016/02/04.10.14_Obama-1170x780.jpg" style="width:100%;height:100%;"/>
+</figure>
+<article>
+	This is article blablabla
+	When people tell me that our President has delivered his 7th State of the Union speech recently, I always correct their mistake and tell it was eighth, in fact. On February 24, 2009, the start of Barack Obama’s 2nd month in office, he made an extensive speech on then ongoing Great Recession crisis. He tried to show what his personal vision for a path out was and why it was aligned with a $787 billion stimulus law that was passed that same day. Also, Mr. President talked foreign politics on that same occasion. He explained how well he will succeed in using diplomacy instead of Bush’s warmongering, strengthen old alliances and, all in all, spruce up US international image to a better one. He said: “living our values doesn’t make us weaker, it makes us safer and it makes us stronger.” Even GOPers were approving of the path POTUS has undertaken then…at least of the rhetorics style, which he so Hollywood-esquely told it in.
+
+</article>
+
+	</content>
+	<aside class="col-4 col-lg-4 main-content">
+		
+aside 			
+aside 		
+aside 		
+aside 	
+aside 
+
+	</aside>
+
+
+</div>
+</div>
+<ul class="post-social">
+<i class="fa fa-facebook-square" aria-hidden="true"></i>
+<i class="fa fa-twitter-square" aria-hidden="true"></i>
+                    <i class="fa fa-instagram" aria-hidden="true"></i>
+                    <i class="fa fa-weibo" aria-hidden="true"></i>
+                    <i class="fa fa-weixin" aria-hidden="true"></i>
+</ul>
+
+
 <?php get_footer(); ?>
