be35fad0c77d9bec2f01d24976b30916e6ad7249
Wang Wenhan <wenhan@outlook.com>
2017-04-03 13:32:01 +1200

    new-style

--- page.php
+++ page.php
@@ -1,4 +1,18 @@
 <?php get_header(); ?>
-
+<div class="container-fluid" style="margin-top:300px">
+    <div class="row">
+        <div class="col-12">
+            <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+            <?php
+			if (have_posts()) :
+                while (have_posts()) :
+                    the_post();
+                    the_content();
+                endwhile;
+            endif;
+            ?>
+        </div>
+    </div>
+</div>
 
 <?php get_footer(); ?>
