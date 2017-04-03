d69e04bd709eaf148568ebe319df29b51b739976
hawkroc <yinpengroc@gmail.com>
2017-04-03 15:47:07 +1200

    'aside'

--- sidebar.php
+++ sidebar.php
@@ -60,8 +60,5 @@
 }(document, 'script', 'facebook-jssdk'));</script>
                 <div style="margin-left:auto;margin-right:auto;margin-bottom:30px">
                     <div class="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="timeline" data-width="500" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>                </div>
-                </div>
-
-                
-            </div>
+               
    
