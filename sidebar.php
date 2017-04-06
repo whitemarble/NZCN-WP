0318f5e4f283d77b4d686cd36540d77f8e44da50
hawkroc <yinpengroc@gmail.com>
2017-04-06 15:35:09 +1200

    'fix'

--- sidebar.php
+++ sidebar.php
@@ -33,19 +33,21 @@
                         </ul>
                     </div>
                 </div> -->
-
-
-                <div id="fb-root"></div>
+<div id="fb-root"></div>
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8";
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
-                <div style="margin-left:auto;margin-right:auto;margin-bottom:30px">
-                    <div class="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="timeline" data-width="500" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>                </div>
-               
+
+<div class="fb-page" 
+  data-href="https://www.facebook.com/NZChinaSociety/"
+  data-width="380" 
+  data-hide-cover="false"
+  data-show-facepile="false" 
+  data-show-posts="false"></div>
    
 
                    <div> 
