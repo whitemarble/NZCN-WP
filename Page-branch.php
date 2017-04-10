ce02b73fb9bf28cbf8f2f04d2af4e7a0a45824d5
hawkroc <yinpengroc@gmail.com>
2017-04-10 19:48:36 +1200

    'fix'

---
+++ Page-branch.php
@@ -0,0 +1,45 @@
+<?php
+/*
+Template Name: branch Page
+*/
+?>
+
+
+<?php get_header(); ?>
+<div class="container-fluid" style="margin-top:8em">
+
+    <div class="row">
+
+        <div class="col-12 col-lg-8 card" style="padding-top: 1em">
+       
+          <ul style="line-height: 3em">
+          	<li style=""><a href="">Auckland News and Events</a></li>
+
+          <li><a href="">Christchurch News and Events</a></li>
+          <li><a href="">Dunedin News and Events</a></li>
+          <li><a href="">Hamilton News and Events</a></li>
+          <li><a href="">Hawkes Bay News and Events</a></li>
+          <li><a href="">Manawatu News and Events</a></li>
+          <li><a href="">Nelson News and Events</a></li>
+          <li><a href="">Rotorua News and Events</a></li>
+          <li><a href="">Taranaki News and Events</a></li>
+          <li><a href="">Tauranga News and Events</a></li>
+          <li><a href="">Timaru News and Events</a></li>
+          <li><a href="">Wairarapa News and Events</a></li>
+          <li><a href="">Wellington News and Events</a></li>
+           <li><a href="">Whanganui News and Events</a></li>
+
+
+          </ul>
+        </div>
+
+        <div class="col-12 col-lg-4 sidebar-right">
+                <?php get_sidebar(); ?>
+
+                
+            </div>
+    </div>
+</div>
+
+
+<?php get_footer(); ?>
